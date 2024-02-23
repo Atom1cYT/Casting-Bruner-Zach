99279a2dc18eb53d5be5727c26179cc863db663c
Atom1cYT <146125539+Atom1cYT@users.noreply.github.com>
2024-02-23 09:08:34 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Casting_Bruner_Zach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asks user to enter a whole number and assigns input to integer
+            Console.WriteLine("Please enter a whole number!");
+           int myFavNum = Convert.ToInt32(Console.ReadLine());
+            // Prints favorite number to console
+            Console.WriteLine("Your favorite number is " + myFavNum);
+            // Asks user a true or false questions and assigns response to boolean
+            Console.WriteLine ("Do you like video games?  (please enter  'true' or 'False')"); 
+            bool isTrue = Convert.ToBoolean(Console.ReadLine());
+            // Print the result of the questions asked
+            Console.WriteLine($"It is {isTrue} that I like video games!");
+        
+        }
+    }
+}
